33132c0bfa4f12f9b8778b88ecddbb9267a8c1c7
babidrummond <babidrummond@hotmail.com>
2024-11-17 01:18:24 -0300

    created profile model, which represents multiple profiles an user may have

--- users/models.py
+++ users/models.py
@@ -2,4 +2,18 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-# class Profile(models.Model):
+class Profile(models.Model):
+    PROFILES_CHOICE = (
+        ('Especialista', 'Profundidade de conhecimento em um campo específico.'),
+        ('Inovador', 'Busca por ideias novas, disruptivas.'),
+        ('Colaborador', 'Prefere compartilhar e discutir ideias.'),
+        ('Comunicador', 'Focado em artigos que podem ser comunicados ao público.'),
+        ('Analista', 'Preocupado com a qualidade dos dados e a metodologia.'),
+    )
+    profile = models.CharField(max_length=100, null=False, choices=PROFILES_CHOICE, verbose_name='profile')
+    research_title = models.CharField(max_length=100, null=False, verbose_name='research title')
+    research_description = models.TextField(max_length=500, null=False, verbose_name='research_description')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='profiles')
+
+    def __str__(self):
+        return self.profile
